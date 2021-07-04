8c82dabb6d0664c109c2dc38ce6a8dd40d70dd0a
larsaars <lurzapps@gmail.com>
2021-07-04 23:08:07 +0200

    probeklausur

---
+++ probeklausur_3g.asm
@@ -0,0 +1,26 @@
+%include "io.inc"
+
+section .data
+Zahl DD 12345
+
+section .text
+
+global CMAIN
+CMAIN:
+    mov ebp, esp; for correct debugging
+    ;write your code here
+    xor eax, eax
+    
+    mov eax, [Zahl]
+    Schleife:
+        xor edx, edx
+        mov ecx, 2
+        div ecx
+        mov ecx, eax
+        PRINT_DEC 4, edx
+        NEWLINE
+        
+        ; macro NEWLINE is too big for short jump -> jnz for long jump -> zero division flag has to be set
+        add ecx, 0
+    jnz Schleife
+    ret
---
+++ probeklausur_3i.asm
@@ -0,0 +1,21 @@
+%include "io.inc"
+
+section .data
+Zahl DD 0x12
+String DB "Die Zahl lautet: %d", 0
+
+section .text
+global CMAIN
+CMAIN:
+    mov ebp, esp; for correct debugging
+    ;write your code here
+    push Zahl
+    push String
+    
+    call printf
+    
+    mov esp, ebp
+    
+    xor eax, eax
+    
+    ret
