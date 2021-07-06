6cbcc1c80643a69d110fa913d232ddc9ecc9974d
larsaars <lurzapps@gmail.com>
2021-07-06 15:17:12 +0200

    probeklausur 3j

---
+++ probeklausur_3j.asm
@@ -0,0 +1,30 @@
+%include "io.inc"
+
+section .data
+Zahl1 DD 0xFFF
+Zahl2 DD 0xFFF
+
+section .text
+
+CarryFlagSet:
+        PRINT_STRING "carry flag is set"
+        ret
+    
+CarryFlatNotSet:
+    PRINT_STRING "carry flag is not set"
+    ret
+
+global CMAIN
+CMAIN:
+    mov ebp, esp; for correct debugging
+    
+        
+    mov eax, [Zahl1]
+    add eax, [Zahl2]
+    
+    
+    jnc CarryFlatNotSet
+    jc CarryFlagSet
+    
+    xor eax, eax
+    ret
