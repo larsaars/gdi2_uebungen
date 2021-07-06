8a28fcb696a520e717dfe39ae74f3580405dfb85
larsaars <lurzapps@gmail.com>
2021-07-06 16:07:33 +0200

    3j

---
+++ probeklausur_3j_with_jc.asm
@@ -0,0 +1,28 @@
+%include "io.inc"
+
+section .data
+Zahl1 DD 0xFFF
+Zahl2 DD 0xFFF
+Fl DD 0x0
+
+section .text
+
+global CMAIN
+CMAIN:
+    mov ebp, esp; for correct debugging
+      
+    mov eax, [Zahl1]
+    add eax, [Zahl2]
+    
+    jc CarryFlagSet
+    
+    xor eax, eax
+    ret
+    
+    CarryFlagSet:
+        PRINT_STRING "carry flag set"
+        xor eax, eax
+        ret
+    
+    xor eax, eax
+    ret
